033b930796f04804bf82ec5e2622e33989c82dc5
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2021-02-24 20:09:52 -0300

    comento app.js para que se entienda mejor

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let funcionalidadTareas = require('./funcionalidadTareas.js');
 
+
+// cuando por consola hagamos "node app.js palabra" palabra va a guardarse en la variable accion y desencadenara distintas operaciones
 let accion = process.argv[2];
+// algunas operaciones requieren informacion adicional, por ejemplo que ingresemos una patente
+// cuando por consola digamos "node app.js buscar patente" esa cuarta palabra que es la patente va en la variable parametroAdicional
 let parametroAdicional = process.argv[3];
+// para saber si una persona puede comprar un auto necesitamos saber el monto maximo que esta dispuesta a pagar y el monto por cuota
+// por eso nos reservamos la quinta posicion y la guardamos en la variable parametroAdicionalDos
+// por ejemplo "node app.js puedeComprar {montoTotal} {montoCuota}"
+// iria como "node app.js puedeComprar 150000000 635000"
 let parametroAdicionalDos = process.argv[4];
 
 switch(accion) {
