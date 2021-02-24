f87fe791c856d2ec0bf8c47c7e060d0c507fd136
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2021-02-24 19:44:40 -0300

    comento el codigo en tareas.js

--- funcionalidadTareas.js
+++ funcionalidadTareas.js
@@ -5,10 +5,12 @@ let concesionaria = {
   autos: autos,
 
   listar: function() {
+      // cuando se llama a este metodo se imprimen todos los autos en autos.json
       console.log(autos);
   },
 
   buscarAuto: function(patenteBuscada) {
+    // cuando se llama a este metodo se imprimen todos los datos del auto con la patente inresada. Sino esta, devuelve null
     console.log('========================')
     console.log('Buscando el auto de patente '+patenteBuscada)
     console.log('========================')
@@ -18,6 +20,7 @@ let concesionaria = {
   },
 
   venderAuto: function(patenteBuscada) {
+    // cuando se llama a este metodo se sobrescribe el objeto de autos, cambiando este auto en particular a vendido
     let autoVendido = this.buscarAuto(patenteBuscada);
     autos = autos.map(function(auto){
       if (auto.patente == patenteBuscada){
@@ -30,6 +33,7 @@ let concesionaria = {
   },
 
   autosParaLaVenta: function() {
+    // cuando se llama a este metodo se imprimen todos los autos en autos.json que su estado vendido es false
     console.log('========================')
     console.log('Estos autos estan en venta:')
     console.log('========================')
@@ -37,6 +41,8 @@ let concesionaria = {
   },
 
   autosNuevos: function() {
+    // cuando se llama a este metodo se imprimen todos los autos en autos.json que su estado vendido es false y sus km son cero
+    // para determinar cuales no estan vendidos se aprovecha como callback del metodo anterior.
     console.log('========================')
     console.log('Estos autos estan en venta y son 0km:')
     console.log('========================')
@@ -44,6 +50,7 @@ let concesionaria = {
   },
 
   listaDeVentas: function() {
+    // cuando se llama a este metodo se imprime un array de precios de autos cuyo estado vendido es true
     let autosVendidos = autos.filter(auto => auto.vendido == true);
     var ventas = []
     autosVendidos.forEach(function(auto){
@@ -53,9 +60,9 @@ let concesionaria = {
   },
 
   totalDeVentas: function() {
+    // cuando se llama a este se suman los precios de todos los autos vendidos
+    // los precios de los autos vendidos se los trae usando como callback el metodo anterior
     let ventas = this.listaDeVentas();
-    //console.log(ventas);
-    //console.log(ventas.length);
     if (ventas.length > 0){
       return ventas.reduce((a, b) => a + b, 0)
     } else {
@@ -64,12 +71,14 @@ let concesionaria = {
   },
 
   puedeComprar: function(auto, persona){
+    // este metodo recibe un objeto auto y un objeto persona, no se puede llamar directamente.
+    // dice true o false si esa persona puede comprar ese auto
     return auto.precio <= persona.capacidadDePagoTotal  && auto.precio / auto.cuotas <= persona.capacidadDePagoEnCuotas
   },
 
   autosQuePuedeComprar: function(persona){
+    // dada una persona este metodo trae todos los autos que esa persona puede pagar
     let autosALaVenta = this.autosParaLaVenta();
-    //console.log(autosALaVenta);
     let autosComprables = autosALaVenta.filter(function(auto){
       return concesionaria.puedeComprar(auto, persona);
     });
